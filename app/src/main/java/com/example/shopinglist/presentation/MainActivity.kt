738ab5b71f3b2cbb7eed4deae7e44ea022fc63f1
Artur Smith <arthur9smith7@gmail.com>
2023-06-26 16:04:25 +0200

    Implemented shopItemActivity. Added methods for adding, editing and setting errors.

--- app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
@@ -2,33 +2,48 @@ package com.example.shopinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.LinearLayout
-import androidx.lifecycle.ViewModel
+import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shopinglist.R
-import com.example.shopinglist.domain.ShopItem
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var recycler: RecyclerView
     private lateinit var recyclerAdapter: RecyclerAdapter
+    private lateinit var buttonAdd: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
         setupRecyclerView()
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setupShopList()
+        addItem()
+        shopItemLongClickListener()
+        editItem()
+        onSwiped()
 
-        viewModel.shopList.observe(this){
-            recyclerAdapter.shopList = it
+    }
+
+    private fun setupShopList() {
+        viewModel.shopList.observe(this) {
+            recyclerAdapter.submitList(it)
         }
     }
 
-    fun setupRecyclerView() {
+    private fun addItem() {
+        buttonAdd.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
+        }
+    }
+
+    private fun setupRecyclerView() {
         with(recycler) {
             adapter = recyclerAdapter
             recycledViewPool.setMaxRecycledViews(RecyclerAdapter.ENABLED_VIEW, RecyclerAdapter.MAX_POOL)
@@ -36,9 +51,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun onSwiped() {
+        val callBack = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = recyclerAdapter.currentList[viewHolder.adapterPosition]
+                viewModel.deleteShopItem(item)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(callBack)
+
+        itemTouchHelper.attachToRecyclerView(recycler)
+    }
+
+    private fun editItem() {
+        recyclerAdapter.shopItemClickListener = {
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
+        }
+    }
+
+    private fun shopItemLongClickListener() {
+        recyclerAdapter.shopItemLongClickListener = {
+            viewModel.editShopItem(it)
+        }
+    }
+
 
     fun initViews() {
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         recycler = findViewById(R.id.recyclerView)
         recyclerAdapter = RecyclerAdapter()
+        buttonAdd = findViewById(R.id.buttonAdd)
     }
 }
